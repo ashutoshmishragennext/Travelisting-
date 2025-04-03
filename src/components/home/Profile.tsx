683e1e56dab88bfc5503df0243862b0f2288a5ce
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-04-03 17:18:31 +0530

    changes

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
-
-export default Profile
-=======
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
