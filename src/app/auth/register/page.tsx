7ab3c20e1d605832a0fe1704681a50fe44a9e3b0
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-04-09 13:20:09 +0530

    register and login page ui changing

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -5,17 +5,16 @@ import React from "react";
 
 function RegisterPage() {
   return (
-    <div className=" w-full min-h-screen bg-blue-50 flex flex-col items-center justify-center bg-cover bg-center relative"
-         
-         >
-         
-      <h1 className="text-5xl mb-8 font-bold">Register</h1>
-      <div className="w-full max-w-md px-4">
-        <RegisterForm text="Create your account" role="USER" />
-      </div>
-      <button className="absolute top-[60px] right-[20px] bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors">
+    <div className="w-full min-h-screen bg-gradient-to-r from-gold-100 to-indigo-50 flex flex-col items-center justify-center px-4">
+      <button className="absolute top-6 right-6 bg-primary text-white px-4 py-2 rounded-lg hover:bg-opacity-90 transition">
         <a href="/auth/login">Login Instead</a>
       </button>
+
+      <h1 className="text-4xl font-bold text-primary mb-6">Register</h1>
+
+      <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-6"> 
+        <RegisterForm text="Create your account" role="USER" />
+      </div>
     </div>
   );
 }
